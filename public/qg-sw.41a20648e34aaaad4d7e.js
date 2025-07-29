0a90f61c413bf19d7035c0f00e8bcc8250897abb
Chen Daniel <daniel.hy.chen@annotsee330.local>
2025-07-29 13:36:17 +0800

    change appid to provided

---
+++ public/qg-sw.41a20648e34aaaad4d7e.js
@@ -0,0 +1,2 @@
+var appId = "41a20648e34aaaad4d7e";
+importScripts("https://aiqua-stg-qgraph.s3-ap-southeast-1.amazonaws.com/v3/r/qg-sw.js");
--- public/qg-sw.8661203bf4223e4bd225.js
+++
@@ -1,2 +0,0 @@
-var appId = "8661203bf4223e4bd225";
-importScripts("https://aiqua-stg-gcp-jp0-web-sdk.s3.ap-northeast-1.amazonaws.com/qgr-http/sdk/v3/r/qg-sw.js");
